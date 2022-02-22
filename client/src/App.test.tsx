7c56250882e4f5e5bbb57984d39f609645b943fb
Matt Mazzola <mattmazzola@live.com>
2022-02-21 20:27:44 -0800

    Fix tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Articles header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const articlesHeader = screen.getByText(/Articles/i);
+  expect(articlesHeader).toBeInTheDocument();
 });
