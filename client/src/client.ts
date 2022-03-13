8eb4b6453b5e11bf42a92e15e9019c65a437e53d
Matt Mazzola <mattmazzola@LIVE.COM>
2022-03-13 21:57:32 +0000

    Merged PR 6: Update base url and build cache key

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,6 +1,6 @@
 import { ArticlesApi, Configuration } from '@dotnetopenapinode/articles'
 
-const baseUrl = `https://localhost:7140`
+const baseUrl = `https://localhost:7002`
 const configuration = new Configuration({ basePath: baseUrl })
 
 export const articlesApi = new ArticlesApi(configuration)
